35c9c0820e8e0538702dbe8e40d3372cac2af0cf
Peter Nguyen <peternguyen160@gmail.com>
2019-02-24 16:27:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  if array.size == 1
+ 	  return array[0]
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  end
+  word_end = array.pop
+  final = array.join(", ")
+  final2 = final+", and #{word_end}"
+  return final2
 
 end
